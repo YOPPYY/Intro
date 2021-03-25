8a20ee84ffd3fa18d348b62f03d33344dc0330a9
TEIKOU <yoshiki444.jp@gmail.com>
2021-03-25 14:10:00 +0900

    0325

--- main.js
+++ main.js
@@ -1,42 +1,83 @@
 // phina.js をグローバル領域に展開
 phina.globalize();
 
-var ASSETS = 'list.js'
-var i=0;
-
+var label=[];
 // MainScene クラスを定義
 phina.define('MainScene', {
   superClass: 'CanvasScene',
   init: function() {
     this.superInit();
-    // アセットローダー
-    var loader = phina.asset.AssetLoader();
+    var self=this;
     // 背景色を指定
     this.backgroundColor = '#444';
+
+    /*
     // ラベルを生成
     this.label = Label('TITLE').addChildTo(this);
     this.label.x = this.gridX.center(); // x 座標
     this.label.y = this.gridY.center()/2; // y 座標
     this.label.fill = 'white'; // 塗りつぶし色
+*/
 
-    var queue=list;
-    console.log(queue)
+    var queue=group1;
+
+    label=Label({
+      x:640,
+      y:25,
+      text:'answer',
+      fill:'white',
+      fontSize:25,
+      align:"right",
+    }).addChildTo(self);
+
+    for(var i=0; i<queue.length; i++){
+      var t=queue[i];
+      label[i]=Label({
+        x:0,
+        y:26+26*i,
+        text:t,
+        fill:'white',
+        fontSize:25,
+        align:"left",
+      }).addChildTo(self);
+    }
+
+    //console.log(queue)
     var button = Button({
       width:200,
       heght:50,
       x:320,
       y:480,
+      text:'再生',
     }).addChildTo(this);
+
     button.onclick=function(){
-
+      if(!queue.length){this.exit();}
       var r= Math.floor(Math.random()*queue.length);
-      console.log(r)
+      //console.log(r)
 
+      var ans = queue.splice(r,1);
+      //var music = new Audio('sound/'+ ans +'.mp3');
+      //music.play();  // 再生
 
-      var music = new Audio('sound/'+list[r].title+'.mp3');
-      music.play();  // 再生
-      queue.splice(r,1);
-      console.log(queue);
+      console.log(ans);
+      label.text=ans;
+      for(var i=0; i<queue.length+1; i++){
+        if(label[i]){label[i].remove();}
+      }
+
+      for(var i=0; i<queue.length; i++){
+        var t=queue[i];
+
+        label[i]=Label({
+          x:0,
+          y:26+26*i,
+          text:t,
+          fill:'white',
+          fontSize:25,
+          align:"left",
+        }).addChildTo(self);
+      }
 
     };
 
